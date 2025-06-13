d8f5b3c77c0ddafccfd7c89a3415370c64f59f5c
JavizzzG <javierstivengalvis2008@gmail.com>
2025-06-13 06:16:19 -0500

    Sistema de registro y login con con token (todavia no fucnional) y ruta de eventos solo con funcion de getAll

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+//DEPENDENCIAS NECESARIAS
+
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import rutasAut from './routes/rutasAut'
+import rutasEventos from './routes/rutasEventos'
+
+//CONFIGURAMOS VARIABLES DE ENTORNO PARA CONECTARNOS A LA DB Y OTRAS CONFIGURACIONES
+dotenv.config()
+const PORT = process.env.PORT || 3001
+const app = express()
+
+//CORS PARA RUTAS Y EXPRESS.JSON PARA DECIR QUE LOS DATOS QUE RECIBIMOS SON EN FORMATO JSON
+app.use(cors())
+app.use(express.json())
+
+//RUTAS DE REGISTRO Y LOGIN
+app.use("/aut", rutasAut)
+
+app.use("/api", rutasEventos) //en proceso
+
+//RUTA DE PRUEBA PARA VERIFICAR QUE EL SERVIDOR ESTÁ CORRIENDO
+// app.get("/api", (_req, res) => {
+//     res.json({message: "API funcionando correctamente oh fajis"})
+// })
+
+
+
+//INICIAMOS EL SERVIDOR
+app.listen(PORT, () => {
+    console.log(`SERVER CORRIENDO EN http://localhost:${PORT}`)
+})
